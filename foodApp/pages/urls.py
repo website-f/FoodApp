cbf65091f87a490bf20f71682e1402d9f579211b
website-f <mohdfitri1017@gmail.com>
2024-05-04 01:42:34 +0800

    version 1.2

--- foodApp/pages/urls.py
+++ foodApp/pages/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard),
+    path('', views.dashboard, name="dashboard"),
     path('order/', views.order),
     path('menu/', views.menu),
+    path('category/', views.category),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout')
 ]
--- foodApp/pages/views.py
+++ foodApp/pages/views.py
@@ -1,11 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Menu, Category, Order
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 
 def order(request):
-    return render(request, 'order.html')
+    orders = Order.objects.all()
+    return render(request, 'order.html', {'orders':orders})
 
 def dashboard(request):
     return render(request, 'index.html')
 
 def menu(request):
-    return render(request, 'menu.html')
+    allMenu = Menu.objects.all()
+    return render(request, 'menu.html', {'allMenu':allMenu})
+
+def category(request):
+    categories = Category.objects.all()
+    return render(request, 'category.html', {'categories':categories})
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            auth_login(request, user)
+            return redirect("dashboard")
+        else:
+            return render(request, 'login.html')
+    else:
+        return render(request, 'login.html')
+    
+def logout(request):
+    auth_logout(request)
+    return redirect("login")
